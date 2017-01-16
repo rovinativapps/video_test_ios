3c91829c3766cf36ef1bdeb160539fe84d6fc33f
mroncalloNativapps <mroncallo@nativapps.mobi>
2017-01-16 16:04:54 -0500

    push

--- PlayVideos/DataService.swift
+++ PlayVideos/DataService.swift
@@ -7,3 +7,96 @@
 //
 
 import Foundation
+import Alamofire
+import SwiftyJSON
+
+
+class DataService{
+    static let sharedInstance = DataService()
+    var videoData = [Data]()
+
+    let baseUrl = "http://media.myvitale.com"
+    
+    func getVideoList(_ cb: @escaping (NSError?, [String]?)->()){
+        
+        //Traer la lista con los nombres de los videos para descargarlos
+        
+        let urlString =  URL(string: "\(baseUrl)/list_exers.json")
+        
+        Alamofire.request(urlString!).validate().responseJSON(completionHandler: {
+            response in
+            
+            switch response.result{
+            case .success:
+                if let value = response.result.value{
+                    let json = JSON(value)
+                    
+                    var listing = [String]()
+                    for  (_,subJson):(String, JSON) in json{
+//                        print(subJson)
+                        let string = "\(subJson["vid_id"].stringValue).mp4"
+                        listing.append(string)
+                    }
+                    
+                    //espera a que la lista termine de almacenar los nombres en un array para continuar
+                    
+                    cb(nil, listing)
+
+                }
+                
+            case .failure(let error):
+                print(error.localizedDescription)
+                cb(error as NSError!, nil)
+            }
+            
+        })
+    }
+    
+    func getVideoFromList(_ list: [String],_ index: Int, _ cb: @escaping (NSError?, [Data]?)->()){
+        //Función recursiva para descargar los videos
+        //recibe la lista de los nombres de los videos a descargar
+        
+        print("list count")
+        print(list.count)
+        
+        
+        if index < list.count{
+            //recorre la lista de los nombres de los videos
+            print("Index: \(index)")
+            let urlString = URL(string: "\(baseUrl)/\(list[index])")
+            Alamofire.request(urlString!).responseData { response in
+                debugPrint("All Response Info: \(response)")
+                
+                switch response.result{
+                case .success:
+                    print("success")
+                    print(response.result.value)
+                    if let data = response.result.value{
+                        self.videoData.append(data)
+                        print("Calling again")
+                        //almacena el video descargado como datos
+                        
+                        //vuelve a llamarse para descargar el próximo video en la lista
+                        self.getVideoFromList(list, index+1, { (error,data) in
+                            cb(error, data)
+                        })
+                    }
+                    
+                    
+                    
+                case .failure(let error):
+                    cb(error as NSError!, nil)
+                }
+                
+            }
+            
+        }else{
+            
+            //regresa la lista de los videos en forma de Data
+            cb(nil, videoData)
+        }
+        
+        print("Video count")
+        print(videoData.count)
+    }
+}
--- PlayVideos/ViewController.swift
+++ PlayVideos/ViewController.swift
@@ -7,12 +7,42 @@
 //
 
 import UIKit
+import AVKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
 
+    var index = 0
+    
+    var videoPlayer: AVPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        //Al aparecer el controlador por primera vez, carga la lista de videos y empieza a descargarlos
+        DataService.sharedInstance.getVideoList { (error, list) in
+            if  (error != nil){
+                print(error!)
+            }else{
+                    print(list!)
+                
+                //Se debe bloquear la pantalla con una barra de descarga para realizar la descarga de todos los videos
+                DataService.sharedInstance.getVideoFromList(list!,0, { data in
+                    //La descarga de los videos finaliza
+                })
+            }
+        }
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        //Cada vez que va a aparecer el controlador se ejecuta esta función
+        self.deleteVideo()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +50,79 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func playVideo(_ sender: UIButton) {
+     
+        // Se crea una ruta para almacenar el video que va a ser visualizado
+        let documents = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let writePath = documents.appending("currentVideo.mp4")
+        
+        
+        let fileExists = FileManager.default.fileExists(atPath: writePath)
+        
+        print(fileExists)
+        if !fileExists{
+            
+            //si el video no existe, se guarda temporalmente en el carrete para poder ser reproducido en la aplicación
+            do{
+                
+                print("trying to save file")
+                
+                
+                //se guarda el video en la ruta especificada
 
+                try DataService.sharedInstance.videoData[self.index].write(to: URL(fileURLWithPath: writePath), options: .atomic)
+                
+                //url local donde está almacenado el video
+                let videoURL = URL(fileURLWithPath: writePath)
+                
+                //Inicialización del reproductor con la url local
+                self.videoPlayer = AVPlayer(url: videoURL)
+                
+                //inicialización
+                let controller = AVPlayerViewController()
+                
+                controller.player = self.videoPlayer
+                
+                //Después de ser almacenado el video se reproduce
+                
+                self.present(controller, animated: true, completion: nil)
+                
+                self.index += 1
+                print("file saved")
+            }catch{
+                print("error saving file")
+                print(error)
+            }
+            
+        }
+        
+        
+        
+        
+    }
+
+    
+    func deleteVideo(){
+
+        //Se borra el video almacenado para evitar que quede en el carrete
+        
+        let documents = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0]
+        let writePath = documents.appending("currentVideo.mp4")
+        do{
+            try FileManager.default.removeItem(atPath: writePath)
+        }catch{
+            print(error)
+        }
+        
+        
+    }
+    
+    func documentsPathForFileName(name: String) -> String {
+        
+        //función para crear la ruta de almacenamiento del video
+        var paths = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)
+        let documentsPath = paths[0]
+        return NSURL(fileURLWithPath: documentsPath).appendingPathComponent(name)!.absoluteString
+    }
 }
 
